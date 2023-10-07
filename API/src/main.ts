4b701966dc20773dd8be074e5acd9c0d53eb401c
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-10-07 16:28:25 -0300

    feat: added useGlobalPipes to main.ts

--- API/src/main.ts
+++ API/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { ValidationPipe } from '@nestjs/common/pipes';
+
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,7 @@ async function bootstrap() {
   const coloredPort = `\x1b[33m\x1b[1m${port}\x1b[0m`;
   const coloredText = `\x1b[32m\x1b[1m]> Server running on port\x1b[0m`;
   const message = `${coloredText} ${coloredPort}`;
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   console.log(message);
 }
