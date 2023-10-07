6689dda91f21550a914a535064aeff3dc5bc7e73
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-10-07 14:41:15 -0300

    feat: created auth module, service, controller. Prisma module is now Global

--- API/src/app.module.ts
+++ API/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, PrismaModule],
-  controllers: [AppController],
+  imports: [UsersModule, ProductsModule, PrismaModule, AuthModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
