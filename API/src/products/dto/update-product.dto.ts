3d447e1ec1b2d07023a6f5840e2dbc345becd75b
cahov <christian.herrera.onaindi@gmail.com>
2023-10-07 18:39:50 -0400

    feat: add products functionality

--- API/src/products/dto/update-product.dto.ts
+++ API/src/products/dto/update-product.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { Exclude } from 'class-transformer';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @Exclude()
+  userId?: string;
+}
--- API/src/products/entities/product.entity.ts
+++ API/src/products/entities/product.entity.ts
@@ -1,1 +1,13 @@
-export class Product {}
+import { $Enums, Product as ProductModel } from '@prisma/client';
+
+export class ProductEntity implements ProductModel {
+  id: string;
+  name: string;
+  description: string;
+  img: string[];
+  currentOffer: number;
+  userId: string;
+  endDate: Date;
+  tags: string[];
+  status: $Enums.Status;
+}
