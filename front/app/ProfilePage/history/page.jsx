48e7a6ce57e10941cecae324c0475a36b1cecc83
Arón Díaz <arondiaz.cpn@gmail.com>
2023-10-25 16:36:24 -0300

    feat: now the user can see all product published on the profile

---
+++ front/app/ProfilePage/history/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import HistorySellingProducts from '@/app/components/HistorySellingProducts/HistorySellingProducts';
+
+const history = () => {
+     return (
+          <div>
+               <HistorySellingProducts />
+          </div>
+     );
+};
+
+export default history;
