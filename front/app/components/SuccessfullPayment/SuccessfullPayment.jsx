53d4c0d64ac7699f0322d85164545c93af4a74af
Arón Díaz <arondiaz.cpn@gmail.com>
2023-10-30 16:50:32 -0300

    updated: styles on successfull payment page

--- front/app/components/SuccessfullPayment/SuccessfullPayment.jsx
+++ front/app/components/SuccessfullPayment/SuccessfullPayment.jsx
@@ -4,16 +4,16 @@ import Link from 'next/link';
 const SuccessfullPayment = () => {
      return (
           <div class="flex items-center justify-center h-screen">
-               <div class="border p-4 shadow-xl rounded-xl">
-                    <h4 class="text-center text-Fern/green text-xl">
+               <div class="border p-4 shadow-2xl rounded-xl">
+                    <h4 class="text-center text-Fern/green text-2xl font-semibold tracking-wide">
                          Tu Dinero fue ingresado con éxito
                     </h4>
-                    <p class="text-center">
+                    <p class="text-center mt-4">
                          Puede tardar unos minutos en verse reflejado dentro de
-                         la web
+                         la web!
                     </p>
                     <Link href="/" class="mt-4 flex justify-center">
-                         <button class="bg-Fern/green rounded-xl px-8 text-lg text-white">
+                         <button class="bg-Fern/green rounded-3xl px-24 py-2 text-md text-white">
                               Volver al inicio
                          </button>
                     </Link>
