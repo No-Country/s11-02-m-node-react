af4e6fd3762bbd7825474166dcc1e972dbb5b9c7
Arón Díaz <arondiaz.cpn@gmail.com>
2023-10-31 10:34:25 -0300

    updated: styles on Success Page

--- front/app/components/SuccessfullPayment/SuccessfullPayment.jsx
+++ front/app/components/SuccessfullPayment/SuccessfullPayment.jsx
@@ -3,17 +3,24 @@ import Link from 'next/link';
 
 const SuccessfullPayment = () => {
      return (
-          <div class="flex items-center justify-center h-screen">
-               <div class="border p-4 shadow-2xl rounded-xl">
-                    <h4 class="text-center text-Fern/green text-2xl font-semibold tracking-wide">
+          <div class="flex mt-20 justify-center ">
+               <div class="p-8 px-4 md:px-20 shadow-2xl rounded-3xl">
+                    <div class="flex justify-center mb-4">
+                         <img
+                              src="https://res.cloudinary.com/dvrmveuzc/image/upload/v1698758726/ptmtkrasu3wxwdumm8ug.png"
+                              alt="payment"
+                         />
+                    </div>
+
+                    <h4 class="text-center text-Fern/green text-xl md:text-2xl font-semibold tracking-wide">
                          Tu Dinero fue ingresado con éxito
                     </h4>
                     <p class="text-center mt-4">
                          Puede tardar unos minutos en verse reflejado dentro de
                          la web!
                     </p>
-                    <Link href="/" class="mt-4 flex justify-center">
-                         <button class="bg-Fern/green rounded-3xl px-24 py-2 text-md text-white">
+                    <Link href="/" class="flex justify-center my-4">
+                         <button class="bg-Fern/green rounded-3xl px-4 md:px-24 py-2 text-sm md:text-md text-white transform hover:scale-105 transition duration-300 ease-in-out">
                               Volver al inicio
                          </button>
                     </Link>
