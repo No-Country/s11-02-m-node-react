0564b98cfe83407d3bbf1e85048d4e9092dd3410
Santiago Aquino <santiagoaquino.dev@gmail.com>
2024-01-23 22:47:13 -0300

    Update route.js

--- front/route.js
+++ front/route.js
@@ -1,6 +1,6 @@
 //Local
 // const mainRoute = `http://localhost:3001`;
 //Deploy
-const mainRoute = `https://reutilizzappapi-bxgb.onrender.com;
+const mainRoute = `https://reutilizzappapi-bxgb.onrender.com`;
 
 export default mainRoute;
